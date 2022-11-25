f220f21ea402b94e9d2d7fcb004b6ba714c2715f
HectorRubio2 <hector.1701710051@ucaldas.edu.co>
2022-11-24 22:04:49 -0500

    permisos

--- app/Http/Middleware/UserAccess.php
+++ app/Http/Middleware/UserAccess.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Permission;
+use App\Models\PermissionRole;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -17,13 +19,38 @@ class UserAccess
     public function handle(Request $request, Closure $next)
     {
         if (!auth('api')->user()) {
-            return response()->json(['You do not have permission to access for this page.'], 401);
+            return response()->json(['no tienes permiso'], 401);
         } else {
 
             $user = auth('api')->user();
-            error_log('usuario>' . $user);
-            error_log('id>' . $user->id);
+            $method = $request->method();
+            $url =  $this->obtenerUrl($request->path());
+            $permission = $this->obtenerPermiso($method, $url);
+            if (is_null($permission))
+                return response()->json(['no existe el permiso'], 404);
+            $permission_role = $this->obtenerPermisoRole($user->role_id, $permission->id);
+            if (is_null($permission_role))
+                return response()->json(['no tienes permiso'], 401);
+
             return $next($request);
         }
+    }
+
+    private  function obtenerUrl($path)
+    {
+        $url = preg_replace('([^A-Za-z/])', '?', $path);
+        $url = preg_replace('(api/)', '', $url);
+        return preg_replace('([?]+)', '?', $url);
+    }
+
+    private function obtenerPermiso($method, $url)
+    {
+        return Permission::where('url', '=', $url)
+            ->where('method', '=', $method)->first();
+    }
+    private function obtenerPermisoRole($role_id, $permission_id)
+    {
+        return PermissionRole::where('role_id', '=', $role_id)
+            ->where('permission_id', '=', $permission_id)->first();
     }
 }
